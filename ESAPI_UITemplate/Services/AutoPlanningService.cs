0c7bc4698542b65343a7502e7acd1c2aacd3d339
mschmidt <mschmidt@gatewayscripts.com>
2023-08-24 08:59:25 -0400

    Add project files.

---
+++ ESAPI_UITemplate/Services/AutoPlanningService.cs
@@ -0,0 +1,36 @@
+﻿using ESAPI_UITemplate.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VMS.TPS.Common.Model.API;
+
+namespace ESAPI_UITemplate.Services
+{
+    public static class AutoPlanningService
+    {
+        public static EsapiWorker EsapiWorker;
+        public static bool SavedModifications;
+        public static StructureSet StructureSet;
+
+        internal static List<StructureModel> GetStructuresFromStructureSet()
+        {
+            List<StructureModel> structures = new List<StructureModel>();
+            EsapiWorker.Run(xapp =>
+            {
+                foreach (var structure in StructureSet.Structures)
+                {
+                    structures.Add(new StructureModel
+                    {
+                        StructureId = structure.Id,
+                        StructureCode = structure.StructureCode?.Code,
+                        DicomType = structure.DicomType,
+                        bIsEmpty = structure.IsEmpty
+                    });
+                }
+            });
+            return structures;
+        }
+    }
+}
---
+++ ESAPI_UITemplate/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using ESAPI_UITemplate.Models;
+using ESAPI_UITemplate.Services;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESAPI_UITemplate.ViewModels
+{
+    public class MainViewModel:BindableBase
+    {
+        private EsapiWorker _esapiWorker;
+        public List<StructureModel> Structures { get; private set; }
+        public MainViewModel(EsapiWorker esapiWorker)
+        {
+            _esapiWorker = esapiWorker;
+            Structures = new List<StructureModel>();
+            GetStructuresFromStructureSet();
+        }
+
+        private void GetStructuresFromStructureSet()
+        {
+            Structures = AutoPlanningService.GetStructuresFromStructureSet();
+        }
+    }
+}
